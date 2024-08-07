ff77dd62c9b4a27d61d6d287da781ec55d199bfb
Edward <qixinlin43@gmail.com>
2024-08-07 18:47:50 -0400

    Test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");   
+        Math(3, 7);
+    }
+
+    public static void Math (int x, int y) {
+        System.out.println(x * y);
     }
 }
